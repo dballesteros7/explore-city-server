26155782028b1af4e4a8a13cfa0d5677a0f6e8df
Diego Ballesteros Villamizar <diegob@student.ethz.ch>
2014-06-24 21:48:44 +0200

    Tour generator server

---
+++ src/python/xplore/tour_server/mock_pyor.py
@@ -0,0 +1,17 @@
+import os
+import random
+
+_CHANCE = 0.3
+
+
+def get_path(start, end):
+    data_file = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir,
+                                             os.pardir, os.pardir, os.pardir,
+                                             'data/valid_images.csv'))
+    results = []
+    with open(data_file, 'r') as images:
+        for line in images:
+            tokens = line.strip().split(',')
+            if random.random() < _CHANCE:
+                results.append(tokens[0])
+    return results
---
+++ src/python/xplore/tour_server/server.py
@@ -0,0 +1,42 @@
+import sys
+
+import cherrypy
+
+try:
+    import pyor
+except:
+    import mock_pyor as pyor
+
+
+class TourService(object):
+    """Controller that produces tours based on two input parameters.
+
+    The input parameters are expected in a JSON-encoded body, two tuples
+    with longitude and latitude of the start and end location of the tour.
+    """
+    exposed = True
+
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def POST(self):
+        params = cherrypy.request.json
+        start_location = params['start_location']
+        end_location = params['end_location']
+        return {'waypoints': pyor.get_path(start_location, end_location)}
+
+
+def run_server():
+    app_conf = {
+        '/': {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}}
+    server_conf = {
+        'server.socket_host': '0.0.0.0',
+        'server.socket_port': 8082,
+        'log.error_file': 'error.log',
+        'log.access_file': 'access.log',
+        'log.screen': False
+    }
+    cherrypy.config.update(server_conf)
+    cherrypy.quickstart(TourService(), '/', app_conf)
+
+if __name__ == '__main__':
+    sys.exit(run_server())
